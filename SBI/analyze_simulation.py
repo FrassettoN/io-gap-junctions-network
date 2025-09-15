c18b4cd0563367beee88ab631e12bee0c847543e
FrassettoN <niccolo.frassetto@gmail.com>
2025-09-15 11:46:01 +0200

    SBI: summary statitics & sims

---
+++ SBI/analyze_simulation.py
@@ -0,0 +1,60 @@
+from scipy.signal import find_peaks
+import numpy as np
+import scipy.signal as signal
+
+
+def compute_firing_rate(spike_times, milliseconds):
+    seconds = milliseconds / 1000.0
+    return len(spike_times) / seconds
+
+
+def compute_sto(v_m, t, spike_times=None, f_range=(5, 12), power_threshold=1e-6):
+    """
+    Compute subthreshold oscillations (STO) in a given frequency range.
+    Returns dominant frequency and amplitude, or None if no oscillation is present.
+    """
+    vm_clean = v_m.copy()
+
+    dt_ms = np.mean(np.diff(t))  # robust dt in ms
+    fs = 1000.0 / dt_ms  # Hz (samples per second)
+
+    # choose nperseg no larger than signal length (and power-of-two often ok)
+    nperseg = min(2048, len(vm_clean))
+    frequencies, power = signal.welch(vm_clean, fs=fs, nperseg=len(vm_clean))
+
+    # Focus on STO frequency range (1–100 Hz)
+    sto_band = (frequencies >= 1) & (frequencies <= 15)
+    sto_freq = frequencies[sto_band][np.argmax(power[sto_band])]
+
+    if sto_freq - 1 > 1e-2:
+        # Find peaks and troughs
+        peaks, _ = find_peaks(vm_clean, distance=400)
+        troughs, _ = find_peaks(-vm_clean, distance=400)
+
+        # Make sure we align peaks/troughs
+        min_len = min(len(peaks), len(troughs))
+        amplitudes = np.abs(vm_clean[peaks[:min_len]] - vm_clean[troughs[:min_len]])
+        mean_amp = np.mean(amplitudes)
+    else:
+        mean_amp = 0
+
+    return sto_freq, mean_amp
+
+
+def analyze(vm, sr, milliseconds):
+    vm_values = vm.events["V_m"]
+    times = vm.events["times"]
+
+    vm_mean = float(np.mean(vm_values))
+
+    sr_spike_times = sr.events["times"]
+    firing_rate = compute_firing_rate(sr_spike_times, milliseconds)
+
+    sto_freq, sto_amp = compute_sto(vm_values, times, sr_spike_times)
+
+    return [
+        round(firing_rate, 2),
+        round(vm_mean, 2),
+        round(sto_freq, 2),
+        round(sto_amp, 2),
+    ]
--- SBI/utils.py
+++ SBI/utils.py
@@ -2,22 +2,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import welch, find_peaks
 
-def plot_vm(vm, show=False, save=False, output="images/V_m"):
+
+def plot_vm(vm, spikes=[], peaks=[], show=False, save=False, output="V_m.png"):
     vm_values = vm.events["V_m"]
     senders = vm.events["senders"]
     times = vm.events["times"]
     plt.figure(figsize=(10, 5))
     for cell_num in np.unique(vm.events["senders"]):
         plt.plot(
-            times[np.where(senders == cell_num)], vm_values[np.where(senders == cell_num)],label=f"Neuron {cell_num}")
-    plt.ticklabel_format(style='plain', axis='y', useOffset=False)
-    plt.legend(loc='upper right')
+            times[np.where(senders == cell_num)],
+            vm_values[np.where(senders == cell_num)],
+            label=f"Neuron {cell_num}",
+        )
+    plt.ticklabel_format(style="plain", axis="y", useOffset=False)
+    plt.legend(loc="upper right")
+    for spike in spikes:
+        plt.axvline(
+            x=spike,
+            linestyle=":",
+            linewidth=1.5,
+            color="red",
+        )
+    for peak in peaks:
+        plt.axvline(
+            x=peak,
+            linestyle=":",
+            linewidth=1.5,
+            color="green",
+        )
     plt.xlabel("time (ms)")
     plt.ylabel("membrane potential (mV)")
     if show:
         plt.show()
     if save:
         plt.savefig(output)
+
 
 def plot_sr(sr, show=False, save=False, output="images/spikes"):
     evs = sr.events["senders"]
@@ -28,8 +47,3 @@ def plot_sr(sr, show=False, save=False, output="images/spikes"):
         plt.show()
     if save:
         plt.savefig(output)
-
-def analyze_vm(vm):
-    vm_values = vm.events["V_m"]
-    return float(np.mean(vm_values))
-
